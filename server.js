68170f02f46e17e7b6a62034d44f27e804b57a06
Lindsay Potrue <lindsaypotrue@comcast.net>
2017-08-30 14:35:40 -0700

    recipes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
-
+const {Recipes} = require('./models');
 const jsonParser = bodyParser.json();
 const app = express();
 
@@ -15,15 +15,19 @@ app.use(morgan('common'));
 // we're going to add some items to ShoppingList
 // so there's some data to look at
 ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
+ShoppingList.create('apples', 3);
 ShoppingList.create('peppers', 4);
-
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar'])
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+  //res.json(ShoppingList.get());
+  res.json({'avacado':'delcious'});
 });
-
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+  //res.json({'avacado':'delcious'});
+});
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
